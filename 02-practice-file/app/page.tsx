41134b1fb635bde454aad0673dd072c9e9253554
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-09-13 13:14:37 +0500

    nextjs-practice

---
+++ 02-practice-file/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col gap-5 justify-center items-center h-[80vh]'>
+      <h1 className='text-8xl font-bold text-center'>Hello World</h1><p className='capititalize text-2xl'>It's My First Next App Hello world!</p>
+    </div>
+  )
+}
+
+export default page
