f81c476a5094fcb202be825f7e9347a0ef2c4f4c
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-09-16 22:55:18 +0500

    modified work

--- 02-practice-file/app/page.tsx
+++ 02-practice-file/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex flex-col gap-5 justify-center items-center h-[80vh]'>
-      <h1 className='text-8xl font-bold text-center'>Hello World</h1><p className='capititalize text-2xl'>It's My First Next App Hello world!</p>
+      <h1 className='text-8xl font-bold text-center '>Hello World</h1><p className='capititalize text-2xl'>It's My First Next Js Hello world!</p>
     </div>
   )
 }
