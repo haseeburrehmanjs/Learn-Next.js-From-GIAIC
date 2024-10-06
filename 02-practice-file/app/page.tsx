afe5e550c04206456cb4f815e402929e33b7fc72
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-10-06 11:24:42 +0500

    modified work

--- 02-practice-file/app/page.tsx
+++ 02-practice-file/app/page.tsx
@@ -31,29 +31,31 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='parentContainer'>
-      <h1 class></h1>
-      <div className='childContainer'>
-        <div className='imgContainer'></div>
-        <h2 className='title'>Golden Black</h2>
-        <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
+    <>
+      <h1 className='text-center mt-5 text-4xl font-bold'>Product Card</h1>
+      <div className='parentContainer'>
+        <div className='childContainer'>
+          <div className='imgContainer'></div>
+          <h2 className='title'>Golden Black</h2>
+          <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
+        </div>
+        <div className='childContainer'>
+          <div className='imgContainer'></div>
+          <h2 className='title'>Golden Black</h2>
+          <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
+        </div>
+        <div className='childContainer'>
+          <div className='imgContainer'></div>
+          <h2 className='title'>Golden Black</h2>
+          <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
+        </div>
+        <div className='childContainer'>
+          <div className='imgContainer'></div>
+          <h2 className='title'>Golden Black</h2>
+          <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
+        </div>
       </div>
-      <div className='childContainer'>
-        <div className='imgContainer'></div>
-        <h2 className='title'>Golden Black</h2>
-        <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
-      </div>
-      <div className='childContainer'>
-        <div className='imgContainer'></div>
-        <h2 className='title'>Golden Black</h2>
-        <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
-      </div>
-      <div className='childContainer'>
-        <div className='imgContainer'></div>
-        <h2 className='title'>Golden Black</h2>
-        <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illo!</p>
-      </div>
-    </div>
+    </>
   )
 }
 
